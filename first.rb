7e3e31672e77af3ccf215d905280f29d48aa4e88
Nepochatova Polina <nepochatova.polina@unicyb.kiev.ua>
2021-09-28 19:06:53 +0300

    Lab2

--- first.rb
+++ first.rb
@@ -1,5 +1,5 @@
-class First
-  $PInum = 3.14
+class FirstLab
+  $Pinum = 3.145
   $Enum = 2.7182
    print "Enter number x:"
    x = gets.chomp.to_i
@@ -7,6 +7,6 @@ class First
    t = gets.chomp.to_i
    print "Enter number z:"
    z = gets.chomp.to_i
-  y = ($Enum ** (Math.tan(x)) ** 2) + (Math.sqrt((t-z).abs)/Math.cos($PInum ** 2) ** 2 + $Enum ** $PInum * z ** 2) + 2.3 * 10 ** 1.6
-  puts y,z
+  y = ($Enum ** (Math.tan(x)) ** 2) + (Math.sqrt((t-z).abs)/Math.cos($Pinum ** 2) ** 2 + $Enum ** $Pinum * z ** 2) + 2.3 * 10 ** 1.6
+  puts y
 end
